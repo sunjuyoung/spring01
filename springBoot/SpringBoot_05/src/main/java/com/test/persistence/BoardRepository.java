65d4c5415641747cb462223773f9fddd8764e5e6
Sun <syseoz@naver.com>
2019-08-12 22:06:29 +0900

    Query

--- springBoot/SpringBoot_05/src/main/java/com/test/persistence/BoardRepository.java
+++ springBoot/SpringBoot_05/src/main/java/com/test/persistence/BoardRepository.java
@@ -2,7 +2,11 @@ package com.test.persistence;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.test.domain.Board;
 
@@ -19,12 +23,62 @@ public interface BoardRepository extends CrudRepository<Board,Long>{
 	//게시글  제목으로 목록을 조회하는 findByTitle()
 	List<Board> findByTitle(String searchKeyword);
 	
-	//Like연산자 특정 단어가 포함된 글 목록 조회
+	//Like연산자 특정 단어가 포함된 글 목록 조회 Containing 키워드를 사용한다.
 	List<Board> findByContentContaining(String content);
 	
 	
+	//여러 조건사용하기 And나 Or 키워드를 결합하여 사용한다
+	//제목 혹은 내용에 특정 단어가 포함된 글 목록을 조회하기 위해
+	List<Board> findByTitleContainingOrContentContaining(String title,String content); 
+	//매개변수 반드시 조건에 맞게해야 한다 , 검색어가 동일하다 하더라도 하나의 변수로 처리할 수 없다.
 	
 	
+	//데이터 정렬
+	List<Board> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+	
+	
+	//모든 쿼리메소드는 마지막 파라미터로 페이징 처리를 위한 Pageable 인터페이스와 정렬을 처리하는 Sort인터페이스를 추가할 수 있따.
+	//페이징 처리
+	//한 화면에 다섯 개의 데이터를 보여주기로 하고 첫 페이지에 해당하는 1번부터 다섯개의 데이터만 조회
+	//List<Board> findByTitleContaining(String searchKeyword,Pageable paging);
+	
+	
+	//Page<T>
+	//웹 페이지에서 페이징 처리에 필요한 다양한 정보를 한번에 얻을 수 있다.
+	Page<Board> findByTitleContaining(String searchKeyword,Pageable paging);
+	
+	//@Query
+	@Query("SELECT b FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")//JPQL검색 대상이 영속성 컨텍스트
+	List<Board> queryAnnotationTest1(String searchKeyword);//searchKeyword가 첫 번째 파라미터 값으로 바인딩된다.
+
+	//JPQL에서는 사용자 입력 값을 바인딩할 수 있도록 위치 기반 파라미터와 이름 기반 파라미터  두 가지를 지원한다.
+	//위치기반 ?1 이라고하면 첫 번쨰 파라미터를 의미한다.
+	
+	
+	
+	//이름기반 @Query    
+	@Query("SELECT b FROM Board b WHERE b.title like %:searchKeyword% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
+	
+	
+	
+	//특정 변수만 조회하기 @Query
+	//검색 결과로 엔티티 객체가 조회되는 것이 아니라 여러 변수 값들이 조회된다 따라서 리턴타입을 List<Object[]>로 해야한다
+	@Query("SELECT b.seq,b.title,b.writer FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
+	List<Object[]>queryAnnotationTest3(@Param("searchKeyword") String searchKeyword);
+	
+	
+	//@Query로 등록한 SQL은 프로젝트가 로딩되는 시점에 파싱되어 처리된다
+	//따라서 SQL오류가 있으면 무조건 예외가 발생되고 프로그램이 실행되지 않는다./
+	//프로그램이 실행되기 전에 사용할 SQL들을 모두 메모리에 올려둠으로써 성능을 향상시킬수 있기 때문이다.
+	
+
+	
+	//네이티브 쿼리
+	//@Query 사용하여 특정 데이터베이스에서만 사용되는 네이티브 쿼리 사용 가능
+	//종속되는 문제가있지만 성능상 특정 데이터베이스에 최적화된 쿼리를 사용해야 하는 경우
+	@Query(value="select seq,title,writer from board where title like '%'||?1||'%' order by seq desc",nativeQuery = true)
+	List<Object[]> queryAnnotationTest4(String searchKeyword);
 	
 
 }
--- springBoot/SpringBoot_05/src/test/java/com/test/QueryMethodTest.java
+++ springBoot/SpringBoot_05/src/test/java/com/test/QueryMethodTest.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -8,48 +9,156 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.test.domain.Board;
 import com.test.persistence.BoardRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QueryMethodTest {
 
-
 	@Autowired
 	private BoardRepository boardRepo;
+
+	/*
+	 * @Before public void dataPrepare() {
+	 * 
+	 * for(int i =1; i<50; i++) { Board board = new Board();
+	 * 
+	 * board.setCnt(0L); board.setTitle(i+" 번째 게시글"); board.setWriter(i+"테스트");
+	 * board.setContent(i+"번쨰 테스트 내용"); board.setCreateDate(new Date());
+	 * boardRepo.save(board); }
+	 * 
+	 * }
+	 */
+
+	/*
+	 * //like 연산자 사용 검색
+	 * 
+	 * @Test public void testFindByTitle() { //List<Board> boardList =
+	 * boardRepo.findByTitle("22 번째 게시글");
+	 * 
+	 * //실행 결과 where 절에서 like연산자 사용 List<Board> boardList =
+	 * boardRepo.findByContentContaining("2");
+	 * 
+	 * System.out.println("검색 결과");
+	 * 
+	 * for(Board board : boardList) { System.out.println("---->" +
+	 * board.toString()); }
+	 * 
+	 * }
+	 */
+
+	/*
+	 * //like or And 검색
+	 * 
+	 * @Test public void testFindByTitleContainingOrContentContaining() {
+	 * 
+	 * List<Board> list = boardRepo.findByTitleContainingOrContentContaining("16",
+	 * "16");
+	 * 
+	 * for(Board board : list) { System.out.println("====>" + board.toString()); }
+	 * 
+	 * 
+	 * Hibernate: select board0_.seq as seq1_0_, board0_.cnt as cnt2_0_,
+	 * board0_.content as content3_0_, board0_.create_date as create_d4_0_,
+	 * board0_.title as title5_0_, board0_.writer as writer6_0_ from board board0_
+	 * where board0_.title like ? escape ? or board0_.content like ? escape ?
+	 * ====>Board(seq=18, title=16 번째 게시글, writer=16테스트, content=16번쨰 테스트 내용,
+	 * createDate=2019-08-11, cnt=0)
+	 * 
+	 * 
+	 * }
+	 */
+
+	/*
+	 * 
+	 * //정렬하기 OrderBy+변수 + Asc or Desc
+	 * 
+	 * @Test public void testOrderby() { List<Board> list =
+	 * boardRepo.findByTitleContainingOrderBySeqDesc("2");
+	 * 
+	 * for(Board board : list) { System.out.println("====>" + board.toString()); } }
+	 * 
+	 * 
+	 */
+
+	/*
+	 * //페이징 정렬
+	 * 
+	 * @Test public void testOrderbyPaging() {
+	 * 
+	 * //Pageable paging = PageRequest.of(0, 3); //페이지 번호, 검색할 데이터의 개수
+	 * 
+	 * Pageable paging = PageRequest.of(0, 5,Sort.Direction.DESC,"seq"); //페이징 처리할때
+	 * 정렬 //쿼리 메소드에 OrderBy를 결합해도 동일한 결과를 얻을 수 있따.
+	 * 
+	 * // List<Board> boardList = boardRepo.findByTitleContaining("게시글", paging);
+	 * 
+	 * //Page<T> Page<Board> pageInfo = boardRepo.findByTitleContaining("게시글",
+	 * paging);
+	 * 
+	 * System.out.println("Page Size : " + pageInfo.getSize());
+	 * System.out.println("Total pages : " + pageInfo.getTotalPages());
+	 * System.out.println("Total count : " + pageInfo.getTotalElements());
+	 * System.out.println("Next : " + pageInfo.nextPageable());
+	 * 
+	 * List<Board> boardList = pageInfo.getContent();
+	 * 
+	 * for(Board board : boardList) { System.out.println("====>" +
+	 * board.toString()); }
+	 * 
+	 * 
+	 * }
+	 * 
+	 */
+
+	/*
+	 * //위치기반 파라미터
+	 * 
+	 * @Test public void testQueryAnnotation() { List<Board> boardList =
+	 * boardRepo.queryAnnotationTest2("10");
+	 * 
+	 * 
+	 * for(Board board : boardList) { System.out.println("====>" +
+	 * board.toString()); }
+	 * 
+	 * }
+	 * 
+	 */
+
+	// @Query 특정 변수만 조회
+	// List<Objectp[]>
 	
-/*	
-	@Before
-	public void dataPrepare() {
-		
-		for(int i =1; i<50; i++) {
-			Board board = new Board();
-		
-			board.setCnt(0L);
-			board.setTitle(i+" 번째 게시글");
-			board.setWriter(i+"테스트");
-			board.setContent(i+"번쨰 테스트 내용");
-			board.setCreateDate(new Date());
-			boardRepo.save(board);
-		}
-		
-	}
-	*/
+	
 	@Test
-	public void testFindByTitle() {
-		//List<Board> boardList = boardRepo.findByTitle("22 번째 게시글");
-		
-		//실행 결과 where 절에서 like연산자 사용
-		List<Board> boardList = boardRepo.findByContentContaining("2");
-		
-		System.out.println("검색 결과");
-		
-		for(Board board : boardList) {
-			System.out.println("---->" + board.toString());
-		}
-		
+	public void testQueryAnnotationtest3() {
+		//List<Object[]> boardList = boardRepo.queryAnnotationTest3("2");
+
+	//	for (Object[] board : boardList) {
+	//		System.out.println("====>" + Arrays.toString(board));
+			/*
+			 * ====>[44, 42 번째 게시글, 42테스트] ====>[34, 32 번째 게시글, 32테스트] ====>[31, 29 번째 게시글,
+			 * 29테스트] ====>[30, 28 번째 게시글, 28테스트] ====>[29, 27 번째 게시글, 27테스트] ....
+			 * 
+			 */
+
+	//	}
+
 	}
+
 	
 	
+	
+	// 네이티브 쿼리
+	@Test
+	public void testQueryAnnotationtest4() {
+		List<Object[]> boardList = boardRepo.queryAnnotationTest4("2");
+
+		// 실행결과를 확인하면 우리가 작성한 SQL구문이 그대로 사용되었다
+		for (Object[] board : boardList) {
+			System.out.println("====>" + Arrays.toString(board));
+
+		}
+
+	}
+
 }
